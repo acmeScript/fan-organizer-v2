484b6af04b022fe216505f4447746cc6761d26d3
peepoo <peepoo@localhost.localdomain>
2020-04-18 18:56:54 +0300

    ui views, firebase BaaS and reactContext features

---
+++ src/features/Sheduler/Sheduler-Content/ShedulerContent.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from "react";
+import { Box } from "grommet";
+import { Page } from "../../../ui/Layout/Page";
+import { useEntity } from "../../../context/loading-context";
+import {useSelectedProjectValue} from '../../../context/selected-project-context';
+import {AddProject} from '../Add-Project';
+import {Projects} from '../Projects';
+import s from "./ShedulerContent.module.css";
+
+
+
+export const ShedulerContent: React.FunctionComponent<{}> = () => {
+    const {setSelectedProject} = useSelectedProjectValue();
+    const [active, setActive] = useState("inbox");
+    const [showProjects, setShowProjects] = useState(true);
+  return (
+    <Page title="Sheduler" documentTitle="Sheduler">
+        <div className={s.sidebar} data-testid="sidebar">
+            <ul className={s.sidebar__generic} style={{display: "flex",flexWrap: "wrap"}}>
+                <li 
+                    onClick={
+                        ()=>{setActive("inbox");setSelectedProject('INBOX')}} 
+                        data-testid="inbox"
+                        className={active === 'inbox' ? s.active : undefined}>
+                            <span>(o)</span>
+                            <span>Почта</span>
+                </li>
+                <li 
+                    onClick={
+                        ()=>{setActive("today");setSelectedProject('TODAY')}} 
+                        data-testid="today" 
+                        className={active === 'today' ? s.active : undefined}>
+                            <span>(o)</span>
+                            <span>Сегодня</span>
+                </li>
+                <li 
+                    onClick={
+                        ()=>{setActive("weak");setSelectedProject('WEAK')}} 
+                        data-testid="weak" 
+                        className={active === 'weak' ? s.active : undefined}>
+                            <span>(o)</span>
+                            <span>Неделя</span>
+                </li>
+                <li 
+                    onClick={
+                        ()=>{setActive("month");setSelectedProject('MONTH')}} 
+                        data-testid="month" 
+                        className={active === 'month' ? s.active : undefined}>
+                            <span>(o)</span>
+                            <span>Месяц</span>
+                </li>
+            </ul>
+            <div className="sidebar__middle" onClick={()=>setShowProjects(!showProjects)}><span></span><h2>Проекты</h2></div>
+            <ul className="sidebar__projects">
+            
+                {showProjects && <Projects />}
+                {showProjects && <AddProject />}
+            </ul>
+        </div>
+    </Page>
+  );
+};
